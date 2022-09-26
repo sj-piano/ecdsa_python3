9870193cd2342a65abc304435b84eaeca3ed2d52
StJohn Piano <stjohnpiano@gmail.com>
2022-09-26 14:21:00 +0200

    __init__.py

---
+++ __init__.py
@@ -0,0 +1,68 @@
+# Imports
+import logging
+
+
+
+
+# Relative imports
+from . import ecdsa
+
+
+
+
+# ### Notes
+# Importing a package essentially imports the package's __init__.py file as a module.
+
+
+
+
+# Collect up the things that we want in the immediate namespace of this module when it is imported.
+# This file allows a parent package to run this:
+# import ecdsa
+# ecdsa.hello()
+# public_key_hex = ecdsa.bitcoin_private_key_hex_to_public_key_hex(private_key_hex)
+hello = ecdsa.code.hello.hello
+validate = ecdsa.util.validate
+configure_module_logger = ecdsa.util.module_logger.configure_module_logger
+#submodules = ecdsa.submodules
+private_key_hex_to_public_key_hex = ecdsa.code.convenience.private_key_hex_to_public_key_hex
+create_deterministic_signature = ecdsa.code.convenience.create_deterministic_signature_low_s
+verify_signature = ecdsa.code.convenience.verify_signature_low_s
+
+
+
+
+# Set up logger for this module. By default, it produces no output.
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
+logger.setLevel(logging.ERROR)
+log = logger.info
+deb = logger.debug
+
+
+
+
+def setup(
+    log_level = 'error',
+    debug = False,
+    log_timestamp = False,
+    log_file = None,
+    ):
+  # Configure logger for this module.
+  ripemd160.util.module_logger.configure_module_logger(
+    logger = logger,
+    logger_name = __name__,
+    log_level = log_level,
+    debug = debug,
+    log_timestamp = log_timestamp,
+    log_file = log_file,
+  )
+  deb('Setup complete.')
+  # Configure modules further down in this package.
+  ripemd160.setup(
+    log_level = log_level,
+    debug = debug,
+    log_timestamp = log_timestamp,
+    log_file = log_file,
+  )
+
